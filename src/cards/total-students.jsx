1c800843d478e0265008578e1de790d61de878ce
Adam Vigneaux <adamvigneaux@gmail.com>
2016-10-08 22:55:00 -0400

    Convert total students to use server data

--- src/cards/total-students.jsx
+++ src/cards/total-students.jsx
@@ -2,20 +2,50 @@ import React from 'react';
 
 import colors from '../colors';
 import DataCard from './data-card.jsx';
-import mockData from '../mock-data';
+import Loader from '../components/loader.jsx';
 import { PieChart } from '../charts/charts';
+import vars from '../vars';
+
+const path = '/statistics/users';
 
 export default class TotalStudents extends DataCard {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        fetch(vars.url + path)
+            .then((response) => response.json())
+            .then((users) => {
+                const usersPercentage = Math.round(users.data.percentage * 100);
+                const notUsersPercentage = 100 - usersPercentage;
+                this.setState({
+                    data: [
+                        {name: 'Current Users',
+                         value: usersPercentage,
+                         unit: '% of students'},
+                        {name: 'Not Users',
+                         value: notUsersPercentage,
+                         unit: '% of students'},
+                    ],
+                    percentage: usersPercentage,
+                });
+            });
+    }
     render() {
-        const chart = (<PieChart data={mockData.totalStudents.data}
-                       colors={[colors.totalStudentsPie]}
-                       backgroundColor={colors.totalStudents}/>);
-        return (
-            <DataCard backgroundColor={colors.totalStudents}
-                      chart={chart}
-                      description="Of Total Students"
-                      size="large"
-                      value={mockData.totalStudents.percentage + '%'} />
-        );
+        if (this.state.percentage) {
+            const chart = (<PieChart data={this.state.data}
+                           colors={[colors.totalStudentsPie]}
+                           backgroundColor={colors.totalStudents}/>);
+            return (
+                <DataCard backgroundColor={colors.totalStudents}
+                          chart={chart}
+                          description="Of Total Students"
+                          size="large"
+                          value={this.state.percentage + '%'} />
+            );
+        } else {
+            return (<DataCard loading={true} />);
+        }
     }
 }
