043d9109ac20a9a71d4425f4c97dfc88b5eb69c9
Adam Vigneaux <adamvigneaux@gmail.com>
2016-10-08 22:36:23 -0400

    Convert popular data card to use server data

--- src/cards/popular-data.jsx
+++ src/cards/popular-data.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Card, CardText, CardTitle } from 'material-ui/Card';
+import { sortBy } from 'underscore';
 
-import mockData from '../mock-data';
 import classes from './data-card.css';
+import Loader from '../components/loader.jsx';
+import vars from '../vars';
+
+const numListItems = 6;
+const path = '/statistics/usage';
 
 const styles = {
     cardText: {
@@ -14,20 +19,53 @@ const styles = {
         fontSize: '2em',
         fontWeight: 500,
     },
+    cardTitleContainer: {
+        paddingBottom: '0',
+    },
 };
 
 export default class PopularData extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    /**
+     * Convert object in form {endpoint: numberOfRequests} to list
+     */
+    componentDidMount() {
+        fetch(vars.url + path)
+            .then((response) => response.json())
+            .then((usage) => {
+                usage = Object.keys(usage.data)
+                    .map((name) => {
+                        return {name: name, value: usage.data[name]};
+                    });
+                usage = sortBy(usage, 'value');
+                usage = usage.reverse()
+                    .slice(0, numListItems);
+                this.setState({usage: usage});
+            });
+    }
     render() {
+        let cardContents = (
+            <Loader />
+        );
+        if (this.state.usage) {
+            cardContents = (
+                <CardText style={styles.cardText}>
+                  <ol>
+                    {this.state.usage.map((el) =>
+                    <li key={el.name}>{el.name}</li>)}
+                </ol>
+                    </CardText>
+            );
+        }
         return (
             <Card className={`${classes.card} ${classes.medium}`}>
               <CardTitle title="Popular Data"
-                         titleStyle={styles.cardTitle} />
-              <CardText style={styles.cardText}>
-                <ol>
-                  {mockData.popularData.data.map((name) =>
-                  <li key={name}>{name}</li>)}
-                </ol>
-              </CardText>
+                         titleStyle={styles.cardTitle}
+                         style={styles.cardTitleContainer}/>
+              {cardContents}
             </Card>
         );
     }
